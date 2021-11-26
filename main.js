6dc56decb82a637d7fb9c6cbc08e093d28a57e87
Fabricio Garcia <gael.fabricio@gmail.com>
2021-11-26 16:31:38 -0600

    ♻Refactor index.js => bookStoarge.js

--- main.js
+++ main.js
@@ -1,31 +1,14 @@
 // eslint-disable-next-line import/extensions
-import * as index from './index.js';
+import * as BookStorage from './bookStorage.js';
 
 const list = document.getElementById('list');
 const addNew = document.getElementById('add-new');
 const contactNav = document.getElementById('contact');
-
-window.onload = () => {
-  if (localStorage.length === 0) {
-    localStorage.setItem('books', JSON.stringify(index.Book.books));
-  }
-  index.Book.displayBooks();
-  index.setDate();
-};
-
 const awesomeBooksTitle = document.getElementById('awesome-books-title');
 const bookStorage = document.getElementById('book-storage');
 const addForm = document.getElementById('add-form');
 const contactTab = document.getElementById('contact-info');
 
-(() => {
-  if (bookStorage.children.length === 0) {
-    awesomeBooksTitle.style.display = 'none';
-    bookStorage.style.display = 'flex';
-    addForm.style.display = 'none';
-    contactTab.style.display = 'none';
-  }
-})();
 const switchTabs = (e) => {
   switch (e.target.id) {
     case 'list':
@@ -57,3 +40,19 @@ const switchTabs = (e) => {
 list.addEventListener('click', (e) => switchTabs(e));
 addNew.addEventListener('click', (e) => switchTabs(e));
 contactNav.addEventListener('click', (e) => switchTabs(e));
+
+window.onload = () => {
+  if (localStorage.length === 0) {
+    localStorage.setItem('books', JSON.stringify(BookStorage.Book.books));
+  }
+
+  if (bookStorage.children.length === 0) {
+    awesomeBooksTitle.style.display = 'none';
+    bookStorage.style.display = 'flex';
+    addForm.style.display = 'none';
+    contactTab.style.display = 'none';
+  }
+
+  BookStorage.Book.displayBooks();
+  BookStorage.setDate();
+};
